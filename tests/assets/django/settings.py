ffebe3532e641439d73534e907771326b7996436
Kumar Aditya <rahuladitya303@gmail.com>
2020-11-16 11:52:17 +0530

    fix(pre-commit): fix mypy error on django

--- tests/assets/django/settings.py
+++ tests/assets/django/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = "123"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
--- tests/assets/django/urls.py
+++ tests/assets/django/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path  # type:ignore
 
 urlpatterns = [
     path("admin/", admin.site.urls),
