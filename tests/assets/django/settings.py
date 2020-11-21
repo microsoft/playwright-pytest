88a4079caac883d8e48ce089ea1e65d05fde2c9b
Kumar Aditya <59607654+kumaraditya303@users.noreply.github.com>
2020-11-22 05:03:00 +0530

    fix(django): configure DJANGO_SETTINGS_MODULE in conftest.py (#24)

--- tests/assets/django/settings.py
+++ tests/assets/django/settings.py
@@ -64,7 +64,7 @@ WSGI_APPLICATION = "proj1.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "NAME": ":memory:",
     }
 }
 
