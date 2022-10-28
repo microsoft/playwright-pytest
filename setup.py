555f100f2c2be0a2bc3dd04e0d18fb201dfa63cd
Anthony Shaw <anthony.p.shaw@gmail.com>
2022-10-28 16:21:50 +1100

    Add trove classifier for 3.11

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
         "Framework :: Pytest",
