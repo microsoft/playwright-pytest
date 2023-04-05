4b470ea608682a37c4e24abfc88366c23e7d7cc1
Christian Jensen <christian@orbik.com>
2023-04-05 01:53:30 -0700

    chore: update requirements to allow the latest `python-slugify` (#160)

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setuptools.setup(
         "playwright>=1.18",
         "pytest>=6.2.4,<8.0.0",
         "pytest-base-url>=1.0.0,<3.0.0",
-        "python-slugify>=6.0.0,<7.0.0",
+        "python-slugify>=6.0.0,<9.0.0",
     ],
     entry_points={"pytest11": ["playwright = pytest_playwright.pytest_playwright"]},
     classifiers=[
