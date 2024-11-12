bbe1e0918e32537833b8980f6a34858b0c1a0fc0
Max Schmitt <max@schmitt.mx>
2024-11-12 23:16:40 +0100

    cleanup

--- tests/test_sync.py
+++ tests/test_sync.py
@@ -19,16 +19,6 @@ import sys
 import pytest
 
 
-@pytest.fixture(autouse=True)
-def _set_default_ini_file(testdir: pytest.Testdir) -> None:
-    testdir.makeini(
-        """
-        [pytest]
-        addopts = -p no:asyncio
-    """
-    )
-
-
 def test_default(testdir: pytest.Testdir) -> None:
     testdir.makepyfile(
         """
